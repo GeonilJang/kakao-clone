6cb3d7cf1ebdb65f2f4d3b321cb98d3a4dad4337
GeonilJang <jgi0105@naver.com>
2018-12-17 16:20:22 +0900

    statge update

--- hide_javascript/todo.js
+++ hide_javascript/todo.js
@@ -4,6 +4,14 @@ const tList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = 'toDos';
 
+function getTime(){
+  const date = new Date();
+  const min = date.getMinutes();
+  const hours = date.getHours();
+  const sec = date.getSeconds();
+  return `${hours < 10 ? `0${hours}` : hours}:${min < 10 ? `0${min}` : min}:${sec < 9 ? `0${sec}` : sec}`;
+}
+
 function loadToDos(){
   const toDos = localStorage.getItem(TODOS_LS);
   if(toDos !== null){
@@ -13,12 +21,24 @@ function loadToDos(){
 
 function paintToDo(text){
   const li = document.createElement('li');
+  const divl = document.createElement('div');
+  const divr = document.createElement('div');
+
   const delBtn = document.createElement("button");
-  delBtn.innerText="X";
+  delBtn.innerText = "x"
+
   const span = document.createElement("span");
   span.innerText = text;
-  li.appendChild(delBtn);
-  li.appendChild(span);
+
+  const spant = document.createElement('span');
+  spant.innerText = getTime();
+
+  divr.appendChild(delBtn);
+  divl.appendChild(spant);
+  divl.appendChild(span);
+
+  li.appendChild(divl);
+  li.appendChild(divr);
   tList.appendChild(li);
 
 
