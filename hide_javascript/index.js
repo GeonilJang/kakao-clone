69a605ad4a46a065537c5adc244e70cf491b2817
GeonilJang <jgi0105@naver.com>
2018-12-19 15:29:29 +0900

    I had enjoyed with it !! thanks Nomad...

--- hide_javascript/index.js
+++ hide_javascript/index.js
@@ -1,18 +1,45 @@
 const title = document.getElementById("title");
 
-const CLICK_CLASS ="clicked";
-
 function handler(){
-  const hasClass = title.classList.contains(CLICK_CLASS);
-  title.classList.toggle(CLICK_CLASS);
-  // if(!hasClass){
-  //   title.classList.add(CLICK_CLASS);
-  // }else{
-  //   title.classList.remove(CLICK_CLASS);
-  // }
+    switch(title.classList[1]){
+      case "clicked":
+        title.classList.remove("clicked");
+        title.classList.add("clicked1");
+        break;
+      case "clicked1":
+        title.classList.remove("clicked1");
+        title.classList.add("clicked2");
+        break;
+      case "clicked2":
+        title.classList.remove("clicked2");
+        title.classList.add("clicked3");
+        break;
+      case "clicked3":
+        title.classList.remove("clicked3");
+        title.classList.add("clicked4");
+        break;
+      case "clicked4":
+        title.classList.remove("clicked4");
+        title.classList.add("clicked5");
+        break;
+      case "clicked5":
+        title.classList.remove("clicked5");
+        title.classList.add("clicked6");
+        break;
+      case "clicked6":
+        title.classList.remove("clicked6");
+        title.classList.add("clicked");
+        break;
+      default:
+        title.classList.add("clicked1");
+        title.classList.remove("clicked");
+        break;
+    }
 }
 
 function init(){
   window.addEventListener("click",handler);
 }
 init();
+
+setInterval(handler,1000)
